e38a742969139f3fb2a94c5e050268c96a181d73
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2023-10-27 00:27:48 +0200

    add tests

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -1,4 +1,39 @@
-from slurm_sweeps.sampler import Grid, Sampler
+import numpy as np
+import pytest
+
+from slurm_sweeps.sampler import Choice, Grid, LogUniform, Sampler, Uniform
+
+
+def test_uniform():
+    parameter = Uniform(0, 2, seed=42)
+    samples = np.array([parameter() for _ in range(100)])
+
+    np.testing.assert_allclose(
+        samples[:3], np.array([1.547912, 0.877757, 1.717196]), rtol=1e-5
+    )
+    np.testing.assert_array_less(samples, 2)
+    np.testing.assert_array_less(0, samples)
+
+
+def test_loguniform():
+    parameter = LogUniform(1, 10, seed=42)
+    samples = np.array([parameter() for _ in range(100)])
+
+    np.testing.assert_allclose(
+        samples[:3], np.array([5.94232, 2.747125, 7.22101]), rtol=1e-5
+    )
+    np.testing.assert_array_less(samples, 10)
+    np.testing.assert_array_less(1, samples)
+
+
+def test_choice():
+    with pytest.raises(AssertionError):
+        Choice([1, "str"])
+
+    parameter = Choice(list(range(5)), seed=43)
+    samples = [parameter() for _ in range(100)]
+    assert samples[:3] == [2, 3, 2]
+    assert all(sample in [0, 1, 2, 3, 4] for sample in samples)
 
 
 def test_grid():
