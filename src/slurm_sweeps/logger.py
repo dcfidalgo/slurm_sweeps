5bbdd846797f445a98e33b5cff8598ee0e0201a6
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2023-11-29 22:52:45 +0100

    update logger with the new database implementation

--- src/slurm_sweeps/logger.py
+++ src/slurm_sweeps/logger.py
@@ -1,42 +1,23 @@
-import json
 import os
 from pathlib import Path
 from typing import Dict, Optional, Union
 
 from .asha import ASHA
-from .constants import (
-    ASHA_PKL,
-    CFG_YML,
-    DB_CFG,
-    DB_ITERATION,
-    DB_LOGGED,
-    DB_PATH,
-    DB_TRIAL_ID,
-    EXPERIMENT_NAME,
-    STORAGE_PATH,
-    TRIAL_ID,
-)
+from .constants import DB_ASHA, DB_PATH, EXPERIMENT_NAME, TRIAL_ID
 from .database import SqlDatabase
-from .storage import Storage
 
 
 class Logger:
     """Log metrics to a slurm sweeps database and cancel trial if ASHA says so."""
 
     def __init__(self):
-        self._experiment_name = os.environ[EXPERIMENT_NAME]
         self._trial_id = os.environ[TRIAL_ID]
+
         if not Path(os.environ[DB_PATH]).is_file():
             raise FileNotFoundError(f"Did not find a database at {os.environ[DB_PATH]}")
-        self._database = SqlDatabase(os.environ[DB_PATH])
+        self._database = SqlDatabase(os.environ[EXPERIMENT_NAME], os.environ[DB_PATH])
 
-        storage = Storage(os.environ[STORAGE_PATH])
-        self._cfg = storage.load(Path(self._trial_id) / CFG_YML)
-        self._asha: Optional[ASHA] = None
-        try:
-            self._asha = storage.load(ASHA_PKL)
-        except FileNotFoundError:
-            pass
+        self._asha: Optional[ASHA] = self._database.load(DB_ASHA)
 
     def log(self, metrics: Dict[str, Union[float, int]], iteration: int):
         """Log metrics to the database.
@@ -58,20 +39,12 @@ class Logger:
                     f"Your metric '{metric}' has type `{type(val)}`."
                 )
 
-        self._database.write(
-            self._experiment_name,
-            {
-                DB_TRIAL_ID: self._trial_id,
-                DB_ITERATION: iteration,
-                DB_CFG: json.dumps(self._cfg),
-                **metrics,
-                **{f"{key}{DB_LOGGED}": 1 for key in metrics.keys()},
-            },
+        self._database.write_metrics(
+            trial_id=self._trial_id, iteration=iteration, metrics=metrics
         )
 
         if self._asha is not None:
-            df = self._database.read(self._experiment_name)
-            df = df[~df[f"{self._asha.metric}{DB_LOGGED}"].isna()]
+            df = self._database.read_metrics(self._asha.metric)
             if self._trial_id in self._asha.find_trials_to_prune(df):
                 raise TrialPruned
 
